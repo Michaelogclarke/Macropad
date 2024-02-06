a1c73dfc3d5ab41eb608fe02eec128b0163eff85
Michaelogclarke <128064847+Michaelogclarke@users.noreply.github.com>
2024-02-06 03:11:44 +0000

    Updated the code.py
    Added consumer controls

--- code.py
+++ code.py
@@ -4,9 +4,17 @@ import board
 import usb_hid
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keycode import Keycode
+from adafruit_hid.consumer_control import ConsumerControl
+from adafruit_hid.consumer_control_code import ConsumerControlCode
 
 keyboard = Keyboard(usb_hid.devices)
 
+consumer = ConsumerControl(usb_hid.devices)
+#App Shortcuts
+notion = Keycode.SHIFT, Keycode.ALT, Keycode.S
+discord = Keycode.SHIFT, Keycode.ALT, Keycode.K
+brave = Keycode.SHIFT, Keycode.ALT, Keycode.L
+spotify = Keycode.SHIFT, Keycode.ALT, Keycode.H
 # Key assaignments
 
 # Top left 
@@ -47,35 +55,36 @@ btn6.direction = digitalio.Direction.INPUT
 btn6.pull = digitalio.Pull.DOWN
 
 
+
 while True:
     if btn1.value:
         print("Button 1 pressed!")
-        keyboard.send(Keycode.CONTROL, Keycode.Y)
-        time.sleep(0.3)
+        consumer.send(ConsumerControlCode.PLAY_PAUSE)
+        time.sleep(0.1)
        
     if btn2.value:
         print("Button 2 pressed!")
-        keyboard.send(Keycode.CONTROL, Keycode.ALT, Keycode.DELETE)
-        time.sleep(0.3)
+        consumer.send(ConsumerControlCode.SCAN_PREVIOUS_TRACK)
+        time.sleep(0.1)
      
     if btn3.value:
         print("Button 3 pressed!")
-        keyboard.send(Keycode.CONTROL, Keycode.X)
-        time.sleep(0.3)
+        consumer.send(ConsumerControlCode.SCAN_NEXT_TRACK)
+        time.sleep(0.1)
       
     if btn4.value:
         print("Button 4 pressed!")
-        keyboard.send(Keycode.CONTROL, Keycode.V)
-        time.sleep(0.3)
+        keyboard.send(Keycode.SHIFT, Keycode.ALT, Keycode.H)
+        time.sleep(0.1)
        
     if btn5.value:
         print("Button 5 pressed!")
-        keyboard.send(Keycode.CONTROL, Keycode.C)
-        time.sleep(0.3)
-        
+        keyboard.send(Keycode.SHIFT, Keycode.ALT, Keycode.S)
+        time.sleep(0.1)
+       
     if btn6.value:
         print("Button 6 pressed!")
-        keyboard.press(Keycode.ALT, Keycode.TAB)
-        keyboard.release_all()
-        time.sleep(0.3)
+        keyboard.send(Keycode.SHIFT, Keycode.ALT, Keycode.L)
+        time.sleep(0.1)
+     
       
